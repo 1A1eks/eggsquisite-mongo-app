13bd6c20da1acf5dd10e79b2801ce7d47c540b7f
1A1eks <aleksander.cornette@gmail.com>
2019-01-02 15:27:55 +0100

    trying to get output4

--- server.js
+++ server.js
@@ -32,13 +32,6 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build'));
-});
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
