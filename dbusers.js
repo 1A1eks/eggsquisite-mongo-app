c8d86d16a058e377544298d99eee502b552c85f4
1A1eks <aleksander.cornette@gmail.com>
2019-01-02 11:47:09 +0100

    wut?

--- dbusers.js
+++ dbusers.js
@@ -1,1 +1,3 @@
-export MONGOLAB_URI="mongodb://farmer:d4b85El@ds026558.mlab.com:26558/the-coop";
+module.exports = { 
+    MONGOLAB_URI="mongodb://farmer:d4b85El@ds026558.mlab.com:26558/the-coop"
+}
